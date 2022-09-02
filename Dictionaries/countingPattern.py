00116957d2af9e731387055bf15cb87ee6786432
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2022-09-02 10:28:15 -0300

    Update countingPattern.py

--- Dictionaries/countingPattern.py
+++ Dictionaries/countingPattern.py
@@ -5,10 +5,14 @@ line = input('')
 
 words = line.split()
 
-print("Words: ", words)
-
-print("Counting...")
 for word in words:
     counts[word] = counts.get(word, 0) + 1
 
 print('Counts', counts)
+
+
+print("\n\n")
+
+jjj = { 'chuck': 1, 'fred': 42, 'jan': 100}
+for k, v in jjj.items():
+    print(k, v)

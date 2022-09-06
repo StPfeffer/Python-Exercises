e8f675532f8a6f705e72ebdfcf3a01769cbb21b8
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2022-09-06 14:35:56 -0300

    Update webBrowser.py

--- Networking/webBrowser.py
+++ Networking/webBrowser.py
@@ -6,11 +6,11 @@ import socket
 mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysocket.connect( ('data.pr4e.org', 80) )
 
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
 mysocket.send(cmd)
 
 while True:
-    data = mysocket.recv(512)
+    data = mysocket.recv(512) # recebe 512 caracteres por vez
     if len(data) < 1:
         break
     print(data.decode())
